8de74c2b861156fa837bea67d72262f41fc89c6a
ArmenYuKh <armencj@yandex.ru>
2021-03-19 09:15:44 +0300

    Creating Spring Boot Web project

---
+++ LetterCounter/src/main/java/com/task/LetterCounter/LetterCounterApplication.java
@@ -0,0 +1,13 @@
+package com.task.LetterCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetterCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetterCounterApplication.class, args);
+	}
+
+}
---
+++ LetterCounter/src/test/java/com/task/LetterCounter/LetterCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.LetterCounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetterCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
